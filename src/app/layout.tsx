0a08b5868c207dcc68407cad32bb7551afc446b9
박종민 <qooktree@naver.com>
2024-03-10 20:35:03 +0900

    Chore: Vercel speed insights 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import Navigation from "@/components/navigation";
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navigation />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
