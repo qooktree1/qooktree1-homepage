6360ba0c5895310ed89d13372ac35cd6da62f9d4
박종민 <qooktree@naver.com>
2024-03-28 17:53:24 +0900

    Feat: First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <h1>Hello</h1>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
